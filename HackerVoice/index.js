fc0f84c60ebe23db066c27160afcaf0ab6158b3a
Shreya <shrgupta@ucdavis.edu>
2021-05-23 18:28:44 -0500

    asdjsf

--- HackerVoice/index.js
+++ HackerVoice/index.js
@@ -6,8 +6,15 @@ module.exports = async function (context, req) {
         ? "Hello, " + password + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    let response_password;
+    if (password === "letmein") {
+        response_password = "Access granted.";
+    } else {
+        response_password = "Access denied.";
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response_password
     };
 }
